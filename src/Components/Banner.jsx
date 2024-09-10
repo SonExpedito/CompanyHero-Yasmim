14e017f87f5f5a7016fc343a5e76a43cd862dcfb
SonExpedito <alexexpeditoff7@gmail.com>
2024-09-10 11:05:09 -0300

    Update-Textos 2/5

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,12 +2,21 @@ import React from "react"
 import BannerImg from "./img/Justin.png"
 
 const Banner = () => {
+    const texto = `
+  Tudo começou quando um chileno - que não falava nada de inglês - e um húngaro - que não falava
+  nada de espanhol - se conheceram no Chile em 2012.
+  Em meio a tantas diferenças, descobriram que tinham uma coisa em comum: a vontade de
+  construir um negócio de sucesso.
+  Inicialmente, fizeram algumas tentativas que acabaram não dando certo, até chegarem na
+  CAMPUS.INC, que basicamente foi a empresa que andou para que a Company Hero pudesse correr.
+`;
+
     return (
         <>
             <div className="h-custombanner w-full rounded-b-2xl flex overflow-hidden  banner ">
                 <div className="w-1/2 h-full flex flex-col justify-center px-20 pb-20 bannertextwrapper  ">
-                    <h1 className="text-4xl uppercase  font-bold italic pb-4 textitles ">Nike</h1>
-                    <p className="text-justify text-lg ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quaerat assumenda dignissimos obcaecati eos ipsa provident amet ipsam sequi enim quibusdam, laudantium a aliquam maxime.</p>
+                    <h1 className="text-4xl uppercase  font-bold italic pb-4 textitles ">Company Hero.</h1>
+                    <p className="text-justify text-lg ">{texto}</p>
                 </div>
                 <div className="w-1/2 h-full flex items-center justify-center bannerimgwrapper  ">
                     <img src={BannerImg} className="h-11/12 rounded-md banner-img" />
--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,66 +1,71 @@
 import React from "react";
-import { FaBusinessTime,FaBuildingUser,FaCalendarCheck,FaEarthAmericas } from "react-icons/fa6";
+import { FaBusinessTime, FaBuildingUser, FaCalendarXmark} from "react-icons/fa6";
+import { FiUserX } from "react-icons/fi";
 
 const Cards = () => {
-
-    
-
+    const Card1 = `Onde
+                    a entrega de 
+                    documentação se torna mais rápida`
+    const Card2 = `Estruturação do processo de
+                    correspondências.`
+    const Card3 = `Durante a reestruturação, encontraram um Locker cheio delas .`
+    const Card4 = `Devido ao atrazo para a notificação gerou uma furia nos clientes.`
 
     return (
         <>
             <div className="h-customcardscontainer  w-full flex flex-col align-center">
-                <h1 className="text-2xl font-bold uppercase text-center textitles my-4">Nossos pontos fortes:</h1>
+                <h1 className="text-2xl font-bold uppercase text-center textitles my-4">Encontrando assim a Solução em dois fatores:</h1>
 
                 <div className="w-full h-full grid md:grid-cols-2  justify-items-center content-center gap-y-12  overflow-hidden ">
 
                     {/**Card */}
-                    <div className="w-80 h-36 bg-gray-200 flex flex-col rounded-xl items-center gap-2">
-                        <h1 className="text-xl font-semibold textitles ">SASS</h1>
-                        <div className="w-full flex">
+                    <div className="w-80 h-36 bg-gray-200 flex flex-col rounded-xl items-center gap-1 py-2">
+                        <h1 className="text-xl font-semibold textitles h-1/6 ">Onboarding</h1>
+                        <div className="w-full flex h-5/6">
                             <div className="w-1/2  flex justify-center items-center">
                                 <FaBusinessTime className="bg-gray-300 rounded-full  h-20 w-20    px-4 py-4" />
                             </div>
                             <div className="w-1/2 flex justify-center items-center">
-                                <p className="font-normal text-justify px-2"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+                                <p className="font-normal text-justify pr-3">{Card1}</p>
                             </div>
                         </div>
                     </div>
 
                     {/**Card */}
-                    <div className="w-80 h-36 bg-gray-200 flex flex-col rounded-xl items-center gap-2 ">
-                        <h1 className="text-xl font-bold textitles ">SASS</h1>
-                        <div className="w-full flex">
+                    <div className="w-80 h-36 bg-gray-200 flex flex-col rounded-xl items-center gap-1 py-2 ">
+                        <h1 className="text-xl font-bold textitles h-1/6 ">Reestruturação</h1>
+                        <div className="w-full flex h-5/6">
                             <div className="w-1/2  flex justify-center items-center">
                                 <FaBuildingUser className="bg-gray-300 rounded-full  h-20 w-20    px-4 py-4" />
                             </div>
                             <div className="w-1/2 flex justify-center items-center">
-                                <p className="font-normal text-justify px-2 "> Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+                                <p className="font-normal text-justify pr-3">{Card2} </p>
                             </div>
                         </div>
                     </div>
 
                     {/**Card */}
-                    <div className="w-80 h-36 bg-gray-200 flex flex-col rounded-xl items-center gap-2">
-                        <h1 className="text-xl font-bold  textitles">SASS</h1>
-                        <div className="w-full flex">
-                            <div className="w-1/2  flex justify-center items-center">
-                                <FaCalendarCheck className="bg-gray-300 rounded-full  h-20 w-20    px-4 py-4" />
+                    <div className="w-80 h-40 bg-gray-200 flex flex-col rounded-xl items-center gap-1 py-2 ">
+                        <h1 className="text-xl font-bold textitles h-1/6 ">TFE’S VENCIDAS</h1>
+                        <div className="w-full flex h-5/6 ">
+                            <div className="w-1/2 h-full  flex justify-center items-center">
+                                <FaCalendarXmark className="bg-gray-300 rounded-full  h-20 w-20    px-4 py-4" />
                             </div>
                             <div className="w-1/2 flex justify-center items-center">
-                                <p className="font-normal text-justify px-2"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+                                <p className="font-normal text-justify pr-1">{Card3} </p>
                             </div>
                         </div>
                     </div>
 
                     {/**Card */}
-                    <div className="w-80 h-36 bg-gray-200 flex flex-col rounded-xl items-center gap-2">
-                        <h1 className="text-xl font-bold  textitles">SASS</h1>
-                        <div className="w-full flex ">
+                    <div className="w-80 h-36 bg-gray-200 flex flex-col rounded-xl items-center gap-1 py-2">
+                        <h1 className="text-xl font-bold  textitles h-1/6">Clientes Furiosos</h1>
+                        <div className="w-full flex h-5/6 ">
                             <div className="w-1/2  flex justify-center items-center">
-                                <FaEarthAmericas className="bg-gray-300 rounded-full  h-20 w-20    px-4 py-4" />
+                                <FiUserX className="bg-gray-300 rounded-full  h-20 w-20    px-4 py-4" />
                             </div>
                             <div className="w-1/2 flex justify-center items-center">
-                                <p className="font-normal text-justify px-2"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+                                <p className="font-normal text-justify pr-2">{Card4}</p>
                             </div>
                         </div>
                     </div>
